be47755f0b7ef2c9a1eb71c995b9476f89e8b58c
nyagamunene <stevenyaga2014@gmail.com>
2026-05-12 12:59:34 +0300

    update variables
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -39,7 +39,6 @@ type config struct {
 	NodeRedURL           string `env:"MG_AGENT_NODERED_URL" envDefault:"http://localhost:1880/"`
 	MqttURL              string `env:"MG_AGENT_MQTT_URL" envDefault:"localhost:1883"`
 	HTTPPort             string `env:"MG_AGENT_HTTP_PORT" envDefault:"9999"`
-	AgentPort            string `env:"MG_AGENT_PORT" envDefault:""`
 	BrokerURL            string `env:"MG_AGENT_BROKER_URL" envDefault:"amqp://guest:guest@localhost:5682/"`
 	MqttSkipTLSVer       string `env:"MG_AGENT_MQTT_SKIP_TLS" envDefault:"true"`
 	MqttMTLS             string `env:"MG_AGENT_MQTT_MTLS" envDefault:"false"`
@@ -201,13 +200,9 @@ func hasBootstrapConfig(cfg config) bool {
 }
 
 func loadEnvConfig(cfg config) (agent.Config, error) {
-	httpPort := cfg.HTTPPort
-	if cfg.AgentPort != "" {
-		httpPort = cfg.AgentPort
-	}
 	sc := agent.ServerConfig{
 		BrokerURL: cfg.BrokerURL,
-		Port:      httpPort,
+		Port:      cfg.HTTPPort,
 	}
 	interval, err := time.ParseDuration(cfg.HeartbeatInterval)
 	if err != nil {
